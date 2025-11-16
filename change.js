cd97858009aa2a84a3e14acfdd6cf6e75afb1ba3
VikashTiwari23 <vikash.tiwari.236968@gmail.com>
2025-11-16 20:39:47 +0530

    Version 2

--- change.js
+++ change.js
@@ -1,2 +1,3 @@
 console.log('change.js');
-console.log('version 1');
+console.log('version 2');
+console.log('you are in folder change.js');
--- coding.js
+++ coding.js
@@ -1,2 +1,3 @@
 console.log("coding.js");
-console.log("version 1");
+console.log("version 2");
+console.log('you are in folder coding.js');
